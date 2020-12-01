86799c1a398a0ffee6f6e071ecedf1d6a38077d0
Luis Gerardo <gerardwjones@gmail.com>
2020-12-01 00:09:21 -0600

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import React from 'react'
+
+const app = () => {
+    return (
+        <h1>
+            Hello there
+        </h1>
+    )
+}
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReacDOM from 'reac-dom'
+import app from './components/app'
+
+ReacDOM.render(<app/>, document.getElementById('app'))
